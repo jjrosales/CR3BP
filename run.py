5957206708255deec459fc18a84a8f07176b6a61
jjrosales <pepe.rosales@gmail.com>
2015-12-26 21:15:17 -0500

    in this commit, the dynamical model is encapulated in a class.

--- run.py
+++ run.py
@@ -14,12 +14,17 @@ from scipy.integrate import ode
 from DynamicModels import CRTBP
 from DynamicModels import CRTBP_Jacobian
 from DynamicModels import P_Jacobian
+from DynamicModels import planar_CRTBP_DynSys
+
 from Constants import *
 
 
+planar_CRTBP = planar_CRTBP_DynSys(MU)
 
-time = 0
+
+time          = 0.0
 target_period = 100.0*6.28
+period        = 0.0
 
 delta_t = 1e-3
 
@@ -30,18 +35,19 @@ D_P          = np.zeros((DIM, DIM), dtype=np.double)
 state_x = []
 state_y = []
 
-state_vector[0] = .9
-state_vector[1] = 1e-1
-state_vector[2] = 1.667478e-1
-state_vector[3] = 0e-1
+#state_vector[0] = .9
+#state_vector[1] = 1e-1
+#state_vector[2] = 1.667478e-1
+#state_vector[3] = 0e-1
 
 #
-#state_vector[0] = 0.84721  
-#state_vector[1] = 0.2
-#state_vector[2] = -.7
-#state_vector[3] =  -0.271 
+state_vector[0] = 1.001  
+state_vector[1] = 0.2
+state_vector[2] = .4
+state_vector[3] =  -0.271 
 
 
+print planar_CRTBP.get_Jacobi_Constant()
 
 
 #sets the integrator
@@ -53,52 +59,72 @@ r.set_initial_value(state_vector, time)
 # The section is defined as x=state_vector[0]
 #
 # we want P(t_f, x(t_f) = x(t_0)
-p_section  = state_vector[0]
+p_section  = state_vector[0] 
 
 
 #integrates until reaches 'target_period'
 
 for i in range(1,30):
 
-    continue_flag = True
-    index = 0
     pos_x = []
     pos_y = []
     vel_x = []
-    vel_y = []    
-    r.set_initial_value(state_vector, time)
+    vel_y = []
     
-    while r.successful() and r.t <= target_period and continue_flag:
+    pos_x.append(state_vector[0])     
+    pos_y.append(state_vector[1])    
+    vel_x.append(state_vector[2])     
+    vel_y.append(state_vector[3])      
+    
+    continue_flag = True
+    index         = 0
+    
+    while time < target_period and continue_flag:
         
+        planar_CRTBP.set_initial_condition(state_vector)
+        planar_CRTBP.set_t0(time)
+        planar_CRTBP.set_tf(time+delta_t)
+    
+        planar_CRTBP.go()
+        
+        state_vector = planar_CRTBP.get_updated_state_vector()
+        time         = planar_CRTBP.get_updated_time()
+        
+       
+        if ((pos_x[-1]-p_section)*(state_vector[0]-p_section)<0.0 and
+            (vel_x[0]*state_vector[2] + vel_y[0]*state_vector[3])>0.0):
+            index = index + 1
+            if index == 1:
+                continue_flag = False
+
         pos_x.append(state_vector[0])     
         pos_y.append(state_vector[1])    
         vel_x.append(state_vector[2])     
         vel_y.append(state_vector[3])
-          
-        state_vector = r.integrate(r.t+delta_t)
-        
-        if (pos_x[-1]-p_section)*(state_vector[0]-p_section)<0.0:
-            index = index + 1
-            if index == 3:
-                continue_flag = False
-    
-    print r.successful()
-            
-    
+                
+
+    # get period
+    period = planar_CRTBP.get_updated_time()
+     
     #Solve for delta_sv
+
+    print planar_CRTBP.get_exec_flag(), state_vector
 
     delta_sv = [pos_x[0], pos_y[0], vel_x[0], vel_y[0]]-state_vector
    
-    D_P =  P_Jacobian(r.t, state_vector)
+#    D_P =  planar_CRTBP.P_Jacobian(time, state_vector)
+    
+    D_P =  P_Jacobian(time, state_vector)
+    
     
     D_P = np.linalg.inv(D_P)
     
     delta_sv = np.dot(D_P, delta_sv)
     
     state_vector = [pos_x[0], pos_y[0], vel_x[0], vel_y[0]] - delta_sv   
-    print delta_sv, state_vector 
-
-
+    time = 0.0
+    print period, delta_sv, state_vector 
+                
     # plotting
     
     plt.annotate('P1', xy=(-MU, 0), xytext=(-MU, -.1))
@@ -109,48 +135,55 @@ for i in range(1,30):
     plt.figure(1)
     plt.plot([p_section, p_section], [-1, 1])
     plt.plot(pos_x, pos_y)
+    plt.plot(pos_x[-1], pos_y[-1], 'go')
     
     plt.figure(2)  
     plt.plot(vel_x, vel_y)    
-    
-    plt.show()
 
+    plt.show()    
+
+
+##
+#target_period = 5.0 * period
+#time = 0.0
 #
-target_period = 2.0 * r.t
+#while time <= target_period:
 #
-r.set_initial_value(state_vector, time)
-while r.successful() and r.t <= target_period:
 #    
-    pos_x.append(state_vector[0])     
-    pos_y.append(state_vector[1])    
-    vel_x.append(state_vector[2])     
-    vel_y.append(state_vector[3])
-#      
-    state_vector = r.integrate(r.t+delta_t)
+#    planar_CRTBP.set_initial_condition(state_vector)
+#    planar_CRTBP.set_t0(time)
+#    planar_CRTBP.set_tf(time+delta_t)
 #
-#
-print r.successful()
-#
-#
-#
-#
-#
-#    # plotting
+#    planar_CRTBP.go()
 #    
-plt.annotate('P1', xy=(-MU, 0), xytext=(-MU, -.1))
-plt.annotate('P2', xy=(-MU, 0), xytext=(1-MU, -.1))
-plt.plot(-MU, 0, 'ro')
-plt.plot(1-MU, 0, 'bo')
+#    state_vector = planar_CRTBP.get_updated_state_vector()
+#    time         = planar_CRTBP.get_updated_time()
+#    
+#    pos_x.append(state_vector[0])     
+#    pos_y.append(state_vector[1])    
+#    vel_x.append(state_vector[2])     
+#    vel_y.append(state_vector[3])
+# 
 #
-#
-plt.figure(1)
-plt.plot([p_section, p_section], [-1, 1])
-plt.plot(pos_x, pos_y)
-#
-plt.figure(2)  
-plt.plot(vel_x, vel_y)    
-#
-plt.show()
+#print '**** time *****', time   
+##
+##    # plotting
+##
+#    
+#plt.annotate('P1', xy=(-MU, 0), xytext=(-MU, -.1))
+#plt.annotate('P2', xy=(-MU, 0), xytext=(1-MU, -.1))
+#plt.plot(-MU, 0, 'ro')
+#plt.plot(1-MU, 0, 'bo')
+##
+##
+#plt.figure(1)
+#plt.plot([p_section, p_section], [-1, 1])
+#plt.plot(pos_x, pos_y)
+##
+#plt.figure(2)  
+#plt.plot(vel_x, vel_y)    
+##
+#plt.show()
 
 
     
